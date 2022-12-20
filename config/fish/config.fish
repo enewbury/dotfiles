50d928c15f135e1a6c87f047bc567a733bc6bfb0
Eric Newbury <eric@newbury.cloud>
2022-12-20 14:02:47 -0500

    updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,20 @@
-set -gx EDITOR (type -p vim)
+fish_add_path /usr/local/sbin
+
+switch (uname)
+  case Linux
+    fish_add_path /home/eric/.local/bin
+    fish_add_path /home/eric/.yarn/bin
+  case Darwin
+    fish_add_path /Users/eric/.local/bin
+    fish_add_path /Users/eric/.yarn/bin
+end
+
+set -gx EDITOR (type -p lvim)
+set -gx GPG_TTY (tty)
+
 alias n lvim
+
 source ~/.asdf/asdf.fish
 source ~/.local/local.fish
+
 starship init fish | source
